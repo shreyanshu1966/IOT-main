8729fdb8db5239f40c4275befd269187ff36ccd4
Shreyanshu Maske <36789736+shreyanshu1966@users.noreply.github.com>
2025-02-12 10:24:12 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*", "allow_headers": "*", "methods": ["GET", "POST", "OPTIONS"], "supports_credentials": True}})
 
 UPLOAD_FOLDER = './uploads'
 if not os.path.exists(UPLOAD_FOLDER):
